d4c87016631cb440cdb4cf9f6755bc3f55c3170f
Juan Ramos <jjuanramos91@gmail.com>
2021-01-30 16:37:37 +0000

    let's see

--- macros/materialize_in_prod.sql
+++ macros/materialize_in_prod.sql
@@ -1,16 +1,17 @@
 {% macro generate_database_name(custom_database_name, node) -%}
 
+    {{ log("Running generate database name macro") }}
     {% set default_database = target.database %}
 
     {% if custom_database_name %}
     
-        {% if target.name in ['prod'] %}
+        {% if target.name in ["prod"] %}
 
-            {% set db_suffix = '-prod' %}
+            {% set db_suffix = "-prod" %}
 
         {% else %}
 
-            {% set db_suffix = '' %}
+            {% set db_suffix = "" %}
 
         {% endif %}
 
@@ -22,4 +23,4 @@
 
     {% endif %}
 
-{%- endmacro %}
+{% endmacro %}
