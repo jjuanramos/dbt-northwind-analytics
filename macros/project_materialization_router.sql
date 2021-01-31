6f468fcac91ecca7ce08ae43d80b77db3c38f644
Juan Ramos <jjuanramos91@gmail.com>
2021-01-31 15:27:41 +0000

    updated macro to better show its meaning

--- macros/project_materialization_router.sql
+++ macros/project_materialization_router.sql
@@ -1,21 +1,18 @@
-{% macro generate_database_name(custom_database_name, node) -%}
+{% macro generate_database_name(custom_database_name=none, node=none) -%}
 
     {{ log("Running generate database name macro") }}
     {% set default_database = target.database %}
+    {% set production_database = 'northwind-analytics-prod' %}
 
     {% if custom_database_name %}
     
         {% if target.name in ["prod"] %}
 
-            {% set db_suffix = "-prod" %}
-
-        {% else %}
-
-            {% set db_suffix = "" %}
+            {{ production_database }}
 
         {% endif %}
 
-        {{ custom_database_name }}{{ db_suffix }}
+        {{ custom_database_name }}
 
     {% else %}
 
